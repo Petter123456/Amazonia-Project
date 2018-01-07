96386ab09841b9c10d5d1c388cda83ea7f5f9b0f
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-01-07 12:43:23 -0500

    stripe gem installed, and implemented withi views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,8 @@ Rails.application.routes.draw do
 
   resources :reviews
   resources :products
+
+  resources :charges
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
