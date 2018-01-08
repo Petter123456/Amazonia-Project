4de1d14ee172aa6a53e8848e47743dfde59c9e1f
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-01-07 23:10:54 -0500

    fixed routes on /login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,22 +4,24 @@ class SessionsController < ApplicationController
     @user = User.new
   end
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to '/login'
-  end
+  # def destroy
+  #   session[:user_id] = nil
+  #   redirect_to '/login'
+  # end
 
 
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      redirect_to root_path
       if params[:remember_me]
         cookies.permanent[:auth_token] = user.auth_token
       else
         flash.now.alert = "Invalid email or password"
-        render "new"
       end
+    else
+      redirect_to '/login'
     end
   end
 
